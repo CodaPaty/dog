29235e6ed202e9ac225dc1a53b4aa49d3b0c63ba
CodaPaty <patriciaduarte20@gmail.com>
2025-08-08 22:52:40 -0300

    adição da classe pessoa

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    private string nome;
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+}
